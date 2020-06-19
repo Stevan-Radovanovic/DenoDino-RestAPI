6f52624bf7d62da46672ae0e5fa1273224958fcd
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-06-19 15:38:35 +0200

    Made simple web server

--- deno-test.ts
+++ deno-test.ts
@@ -1,9 +1,19 @@
 let message = "Testing Deno...";
 console.log(message);
 
+import { serve } from "https://deno.land/std/http/server.ts";
+
 let textForFile = "This text should be stored in a file using Deno Runtime API";
 let encoder = new TextEncoder();
 let data = encoder.encode(textForFile);
 
 let result = await Deno.writeFile("file.txt", data);
-console.log("File has been made");
+console.log("File has been made!");
+
+console.log("Now onto starting a server...");
+
+const server = serve({ port: 3000 });
+console.log("Starting server");
+for await (const req of server) {
+  req.respond({ body: "Hello World\n" });
+}
