4d1cf82aca9751b799c1bc99bf624d032daf22c8
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-06-19 23:52:46 +0200

    Added headers middleware

--- deno-dino/app.ts
+++ deno-dino/app.ts
@@ -3,6 +3,16 @@ import routes from "./routes.ts";
 
 const app = new Application();
 
+app.use(async (ctx, next) => {
+  ctx.response.headers.set("Access-Control-Allow-Origin", "*");
+  ctx.response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE",
+  );
+  ctx.response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  await next();
+});
+
 app.use(routes.routes());
 app.use(routes.allowedMethods());
 
