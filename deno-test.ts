03be90fca68f7cf1d06dd36fcceafe50561c9129
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-06-19 15:28:54 +0200

    Added writing to a file

--- deno-test.ts
+++ deno-test.ts
@@ -1,2 +1,9 @@
-let message = "Testing Deno commands...";
+let message = "Testing Deno...";
 console.log(message);
+
+let textForFile = "This text should be stored in a file using Deno Runtime API";
+let encoder = new TextEncoder();
+let data = encoder.encode(textForFile);
+
+let result = await Deno.writeFile("file.txt", data);
+console.log("File has been made");
